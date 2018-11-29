82b297a8d525e26a4edf9735f107153bcb860e1b
andrewjyang <ayang2@zagmail.gonzaga.edu>
2018-11-28 21:12:08 -0800

    Changes to first pull request and created a Prescription Row button

--- WebPhapp/client/src/App/pages/Home.js
+++ WebPhapp/client/src/App/pages/Home.js
@@ -5,10 +5,15 @@ class Home extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Project Home</h1>
+        <h1>Pharmaceutical Blockchain</h1>
         {/* Link to List.js */}
         <Link to={"./list"}>
           <button variant="raised">My List</button>
+        </Link>
+        <br />
+        <br />
+        <Link to={"./prescription"}>
+          <button variant="raised">Prescription Row</button>
         </Link>
       </div>
     );
