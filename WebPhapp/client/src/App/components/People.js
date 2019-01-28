ebf15c089e0df3228f6a78c6d9edf5ac67898bd7
andrewjyang <ayang2@zagmail.gonzaga.edu>
2019-01-28 11:31:38 -0800

    bootstrap frontend with argon; fixed front end testing

--- WebPhapp/client/src/App/components/People.js
+++ WebPhapp/client/src/App/components/People.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+// import argon from "argon-design-system-free";
 
 class People extends Component {
 
@@ -13,7 +14,7 @@ class People extends Component {
           {person.patient_id}</a>
           </td>
           <td>
-            <a href = {"/patient?ID=" + person.patient_id} target="_blank">
+            <a href = {"/patient?ID=" + person.patient_id}>
               {person.first}
             </a>
           </td>
@@ -34,19 +35,22 @@ class People extends Component {
 
   render() {
     return (
-      <table class="table table-hover">
-      <tbody>
-        <tr class="table-primary">
-          <th scope="col">ID</th>
-          <th scope="col" >First Name</th>
-          <th scope="col" >Last Name</th>
-          <th scope="col">Date Of Birth</th>
-        </tr>
-
-        {this.displayPeople()}
-
-      </tbody>
-      </table>
+      <div className="table-responsive">
+        <br/>
+        <table className="table align-items-center">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Date Of Birth</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.displayPeople()}
+        </tbody>
+        </table>
+      </div>
     );
   }
 }
--- WebPhapp/client/src/App/pages/PatientSearch.js
+++ WebPhapp/client/src/App/pages/PatientSearch.js
@@ -46,33 +46,95 @@ class PatientSearch extends Component {
   render() {
 
     return (
-      <div className="form-group">
-        <h1>Patient Search </h1>
-        <form>
-
-          <label>
-            First Name: &nbsp;
-          </label>
-          <input
-            type="p"
-            value={this.state.firstName}
-            onChange={this.onKeyDownFirstName}
-          />
-
-          <label>
-            &nbsp; Last Name: &nbsp;
-          </label>
-          <input
-            type="p"
-            value={this.state.lastName}
-            onChange={this.onKeyDownLastName}
-          />
+        <div className="form-group">
+          <nav className="navbar navbar-horizontal navbar-expand-lg navbar-dark bg-primary">
+            <div className="container">
+                <a className="navbar-brand" href="#">Patient Lookup</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbar-default">
+                    <div className="navbar-collapse-header">
+                      <div className="row">
+                          <div className="col-6 collapse-brand">
+                              <a href="../../index.html">
+                                  <img src="../../../public/assets/img/brand/blue.png"/>
+                              </a>
+                          </div>
+                          <div className="col-6 collapse-close">
+                              <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false" aria-label="Toggle navigation">
+                                  <span></span>
+                                  <span></span>
+                              </button>
+                          </div>
+                      </div>
+                    </div>
+                    <ul className="navbar-nav ml-lg-auto">
+                        <li className="nav-item">
+                            <a className="nav-link nav-link-icon" href="#">
+                                <i className="ni ni-favourite-28"></i>
+                                <span className="nav-link-inner--text d-lg-none">Discover</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link nav-link-icon" href="#">
+                                <i className="ni ni-notification-70"></i>
+                                <span className="nav-link-inner--text d-lg-none">Profile</span>
+                            </a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link nav-link-icon" href="#" id="navbar-default_dropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i className="ni ni-settings-gear-65"></i>
+                                <span className="nav-link-inner--text d-lg-none">Settings</span>
+                            </a>
+                            <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbar-default_dropdown_1">
+                                <a className="dropdown-item" href="#">Action</a>
+                                <a className="dropdown-item" href="#">Another action</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <br/>
+        <form role="form">
+          <div class="form-group mb-3">
+            <div class="input-group input-group-alternative">
+              <div class="input-group-prepend">
+                <span class="input-group-text"></span>
+              </div>
+              <input 
+                class="form-control" 
+                placeholder="First Name" 
+                type="p"
+                value={this.state.firstName}
+                onChange={this.onKeyDownFirstName}
+              />
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="input-group input-group-alternative">
+              <div class="input-group-prepend">
+                <span class="input-group-text"></span>
+              </div>
+              <input 
+                class="form-control" 
+                placeholder="Last Name" 
+                type="p"
+                value={this.state.lastName}
+                onChange={this.onKeyDownLastName}
+              />
+            </div>
+          </div>
+          <div class="text-center">
+            <button type="button" className="btn btn-primary my-4"
+              onClick={this.onSearchPatients}>
+              Search
+            </button>
+          </div>
         </form>
-
-          <button type="button" class="btn btn-info" onClick={this.onSearchPatients}>
-            Search
-          </button>
-
         <People patientList={this.state.people}/>
       </div>
     );
